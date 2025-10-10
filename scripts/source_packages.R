fe27ed5a8c3f47cd97cf005782936b76b8ff2d93
Gicquel <Gicquel@izw-berlin.local>
2025-10-10 11:17:06 +0200

    adding script for making maps of area and environmental covariates

--- scripts/source_packages.R
+++ scripts/source_packages.R
@@ -3,9 +3,9 @@
 
 mylibraries <- c("dplyr", "tidyr",  "data.table", "units", "stringr", "phyloseq", "abind", "here", "forcats",
                  "ggplot2", "corrplot", "viridis", "ggpubr", "ggsankey", "RColorBrewer", "officedown", "flextable", "extrafont", "cowplot",
-                 "ggcorrplot", "ggrepel",
+                 "ggcorrplot", "ggrepel", "ggspatial",
                  "ggraph", "igraph", "shades",
-                 "tmap", "ggmap", "terra", "sf", "ggdensity", "rnaturalearth",
+                 "tmap", "ggmap", "terra", "tidyterra", "sf", "ggdensity", "rnaturalearth",
                  "dismo", "Hmsc", "MCMCvis", "tictoc")
 
 for (i in 1:length(mylibraries)) {
