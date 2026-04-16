cceee1c39eaae93a808e85923222b7c0c268a371
Morgane Gicquel <72741950+mgicquel@users.noreply.github.com>
2026-04-16 15:04:23 +0200

    Delete scripts/source_api.R

--- scripts/source_api.R
+++
@@ -1,8 +0,0 @@
-
-### Google API key
-
-register_google(key = "AIzaSyCwlMyqmj25UhB-fwlgfkei8JR127xkWZQ", write = TRUE)
-
-register_stadiamaps("86aa80cc-9be5-4df2-987b-c67a83921026", write = TRUE)
-
-
